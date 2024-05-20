396f73fac942862f2f8c4b1116b84290eb44d016
Gianni Carafa <gianni.carafa@srf.ch>
2024-05-20 19:59:01 +0200

    initial commit for kubero kpacks deployer

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # deployer
+
+Deployer is an image to triger kpax builds, wait till the image is built and then deploy it to a kubernetes cluster.
+
+## install kpacks
---
+++ init.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+
+kubectl apply -f - <<EOF
+apiVersion: kpack.io/v1alpha2
+kind: Build
+metadata:
+  name: ${NAME}
+  kubero.dev/app: ${APP}
+  kubero.dev/phase: ${PHASE}
+  kubero.dev/pipeline: ${PIPELINE}
+spec:
+  tags:
+  - ${TAG}
+  serviceAccountName: ${SERVICE_ACCOUNT}
+  builder:
+    image: ${BUILDER}
+  source:
+    git:
+      url: ${URL}
+      revision: ${REVISION}
+EOF
+
+# check if kubectl command failed
+if [ $? -ne 0 ]; then
+  echo "Failed to create build pod"
+  exit 1
+fi
+
+sleep 2
+
+## check with kubectl if the pod is completed
+while true; do
+  PHASE=$(kubectl get pod ${NAME}-build-pod -o jsonpath='{.status.phase}')
+  if [ "$PHASE" == "Succeeded" ]; then
+    echo "Build completed successfully"
+    break
+  fi
+  if [ "$PHASE" == "Failed" ]; then
+    echo "Build failed"
+    break
+  fi
+  echo "Waiting for build to complete...$PHASE"
+  sleep 1
+done
+
+sleep 2
+
+# patch kuberoes resource with the new image
+kubectl patch kuberoes ${APP} -p "{\"spec\":{\"image\":\"${IMAGE}\"}}"
+if [ $? -ne 0 ]; then
+  echo "Failed to patch kuberoes resource"
+  exit 1
+fi
+
+exit 0
